5fb133f8bf137f37e710700cbc83f8db1461b98a
SimoSultan <simosultan2020@gmail.com>
2021-01-10 12:03:48 +1000

    added loading symbol and error messsage to product page

--- curtainco/src/components/products/Products.js
+++ curtainco/src/components/products/Products.js
@@ -1,62 +1,67 @@
-import React, { useEffect, useState } from "react";
-import { Container, Grid } from "@material-ui/core";
-import ProductList from "./product/ProductList";
-import Search from "./sidebar/Search";
-import Filter from "./sidebar/Filter";
-import Sort from "./sidebar/Sort";
-import useStyles from "./ProductStyles";
-import { useCurtainContext } from "../../config/CurtainCoContext";
-import { ACTIONS } from "../../config/stateReducer";
-import { sortACTIONS, sortProducts } from "../../helpers/productHelpers";
-import { getAllProducts } from "../../services/productServices";
+import React, { useEffect, useState } from "react"
+import { Container, Grid } from "@material-ui/core"
+import ProductList from "./product/ProductList"
+import Search from "./sidebar/Search"
+import Filter from "./sidebar/Filter"
+import Sort from "./sidebar/Sort"
+import useStyles from "./ProductStyles"
+import { useCurtainContext } from "../../config/CurtainCoContext"
+import { ACTIONS } from "../../config/stateReducer"
+import { sortACTIONS, sortProducts } from "../../helpers/productHelpers"
+import { getAllProducts } from "../../services/productServices"
+import LoadingSymbol from "../reusable/LoadingSymbol"
 
-const sortFields = Object.values(sortACTIONS);
+const sortFields = Object.values(sortACTIONS)
 
 function Products() {
-    const classes = useStyles();
-    const { state, dispatch } = useCurtainContext();
-    const [sortBy, setSortBy] = useState(sortACTIONS.NAME_ALPHABETICAL);
-    const [searchInput, setSearchInput] = useState("");
+    const classes = useStyles()
+    const { state, dispatch } = useCurtainContext()
+    const [sortBy, setSortBy] = useState(sortACTIONS.NAME_ALPHABETICAL)
+    const [searchInput, setSearchInput] = useState("")
+    const [isLoading, setIsLoading] = useState(true)
+    const [productsErrorMessage, setProductErrorMessage] = useState(null)
     const [filter, setFilter] = useState({
         fabric: false,
         track: false,
         accessory: false,
-    });
+    })
 
     // HANDLE THE STATE CHANGE FOR FILTERING
     const handleFilterChange = (event) => {
-        setFilter({ ...filter, [event.target.name]: event.target.checked });
-    };
+        setFilter({ ...filter, [event.target.name]: event.target.checked })
+    }
 
     // HANDLE THE STATE CHANGE FOR FILTERING
     const handleSortByChange = (event) => {
-        setSortBy(event.target.value);
-    };
+        setSortBy(event.target.value)
+    }
 
     const handleSearchInputChange = (event) => {
-        setSearchInput(event.target.value);
-    };
+        setSearchInput(event.target.value)
+    }
 
     useEffect(() => {
         getAllProducts()
             .then((resp) => {
                 if (resp.status === 200) {
-                    console.log("---PRODUCTS---");
-                    console.log(resp.data);
+                    console.log("---PRODUCTS---")
+                    console.log(resp.data)
                     let sortedProducts = sortProducts(
                         resp.data,
                         sortACTIONS.NAME_ALPHABETICAL
-                    );
+                    )
                     dispatch({
                         type: ACTIONS.SET_ALL_PRODUCTS,
                         payload: sortedProducts,
-                    });
+                    })
                 }
             })
             .catch((error) => {
-                console.log(error);
-            });
-    }, [dispatch]);
+                console.log(error)
+                setProductErrorMessage(error)
+            })
+        setIsLoading(false)
+    }, [dispatch])
 
     return (
         <>
@@ -100,19 +105,25 @@ function Products() {
                         justify="center"
                         alignItems="center"
                     >
-                        <ProductList
-                            products={state.products}
-                            filterText={searchInput}
-                            filterTypes={filter}
-                            filterSortBy={sortBy}
-                            sortFields={sortFields}
-                            inStockOnly={true}
-                        />
+                        {isLoading ? (
+                            <LoadingSymbol />
+                        ) : productsErrorMessage !== null ? (
+                            productsErrorMessage
+                        ) : (
+                            <ProductList
+                                products={state.products}
+                                filterText={searchInput}
+                                filterTypes={filter}
+                                filterSortBy={sortBy}
+                                sortFields={sortFields}
+                                inStockOnly={true}
+                            />
+                        )}
                     </Grid>
                 </Container>
             </Grid>
         </>
-    );
+    )
 }
 
-export default Products;
+export default Products
