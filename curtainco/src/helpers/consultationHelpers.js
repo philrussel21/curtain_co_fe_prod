7093f20ed6a2a8b58668e69d7e257b76e457467c
SimoSultan <simosultan2020@gmail.com>
2021-01-11 13:36:03 +1000

    removed referrer meta tag on index.html

--- curtainco/src/helpers/consultationHelpers.js
+++ curtainco/src/helpers/consultationHelpers.js
@@ -1,25 +1,25 @@
 function sortConsultations(allConsults) {
-    // sort by date, newest first
+    // sort by date, oldest first
     // then sort by processed, showing unprocessed first
     let sorted = allConsults.sort((a, b) => {
         // Turn your strings into dates, and then subtract them
         // to get a value that is either negative, positive, or zero.
-        return new Date(b.createdAt) - new Date(a.createdAt);
-    });
+        return new Date(a.createdAt) - new Date(b.createdAt)
+    })
 
-    let completedConsults = [];
-    let incompleteConsults = [];
+    let completedConsults = []
+    let incompleteConsults = []
 
     for (let i = 0; i < sorted.length; i++) {
-        const element = sorted[i];
+        const element = sorted[i]
         if (element.isProcessed) {
-            completedConsults.push(element);
+            completedConsults.push(element)
         } else {
-            incompleteConsults.push(element);
+            incompleteConsults.push(element)
         }
     }
 
-    return [...incompleteConsults, ...completedConsults];
+    return [...incompleteConsults, ...completedConsults]
 }
 
-module.exports = { sortConsultations };
+module.exports = { sortConsultations }
